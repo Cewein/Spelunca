ea7a9a70f4f3ded7aaf54e9e83729e62fb5c590a
Maximilien Nowak <pro.mnowak@gmail.com>
2019-11-18 18:49:58 +0100

    second jump

--- Assets/script/MarchingCube.cs
+++ Assets/script/MarchingCube.cs
@@ -17,23 +17,23 @@ public class MarchingCubeMaster : MonoBehaviour
 
     public void Awake()
     {
-        meshFilter = gameObject.AddComponent<MeshFilter>();
+        //meshFilter = gameObject.AddComponent<MeshFilter>();
     }
 
     public void createMarchingBlock()
     {
-        float[,,] block = new float[size, size, size];
+        float[,,] block = new float[size+1, size+1, size+1];
 
         MeshData masterMeshData = new MeshData();
 
         Vector3 pos = GetComponent<Transform>().position;
 
-        block = DensityGenerator.find(block, 0, size, pos);
-        for (int x = 0; x < size - 1; x++)
+        block = DensityGenerator.find(block, 0, size + 1, pos);
+        for (int x = 0; x < size; x++)
         {
-            for (int y = 0; y < size - 1; y++)
+            for (int y = 0; y < size; y++)
             {
-                for (int z = 0; z < size - 1; z++)
+                for (int z = 0; z < size; z++)
                 {
                     MeshData meshData = MeshGenerator.generateMesh(block, x, y, z,size, floor,masterMeshData.tcount);
                     masterMeshData.mergeMeshData(meshData);
