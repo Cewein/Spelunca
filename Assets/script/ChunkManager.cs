9f3a6518eba10a9a932b55b33f1dda4c3a77adbf
Cewein <pro.mnowak@gmail.com>
2019-12-31 01:29:58 +0100

    moving world gen
    it move around the player

--- Assets/script/ChunkManager.cs
+++ Assets/script/ChunkManager.cs
@@ -20,19 +20,7 @@ public class ChunkManager : MonoBehaviour
         playerChunk = new Vector3();
         chunks = new GameObject[viewRange,viewRange,viewRange];
 
-        int half = (int)viewRange / 2;
-
-        for (int x = 0; x < viewRange; x++)
-        {
-            for (int y = 0; y < viewRange; y++)
-            {
-                for (int z = 0; z < viewRange; z++)
-                {
-                    chunks[x, y, z] = Instantiate(chunk,new Vector3(x-half,y-half,z-half) * chunkSize, new Quaternion());
-                    chunks[x, y, z].GetComponent<chunk>().createMarchingBlock(chunkSize);
-                }
-            }
-        }
+        generateChunks();
     }
 
     void Start()
@@ -54,17 +42,68 @@ public class ChunkManager : MonoBehaviour
         {
             print("in a new chunk");
             print("new chunk is : " + playerChunk.ToString());
+            Vector3 direction = (temp - playerChunk);
             playerChunk = temp;
+            updateChunks(direction);
         }
     }
 
     void generateChunks()
     {
+        int half = (int)viewRange / 2;
 
+        for (int x = 0; x < viewRange; x++)
+        {
+            for (int y = 0; y < viewRange; y++)
+            {
+                for (int z = 0; z < viewRange; z++)
+                {
+                    chunks[x, y, z] = Instantiate(chunk, new Vector3(x - half, y - half, z - half) * chunkSize, new Quaternion());
+                    chunks[x, y, z].GetComponent<chunk>().createMarchingBlock(chunkSize);
+                }
+            }
+        }
     }
 
-    void updateChunks()
+    void updateChunks(Vector3 direction)
     {
+        for (int x = 0; x < viewRange; x++)
+        {
+            for (int y = 0; y < viewRange; y++)
+            {
+                for (int z = 0; z < viewRange; z++)
+                {
+                    Vector3 chunkArrayPosition = new Vector3(x, y, z);
 
+                    if (inArray(chunkArrayPosition + direction))
+                    {
+                        int xb = (int)direction.x;
+                        int yb = (int)direction.y;
+                        int zb = (int)direction.z;
+
+                        chunks[x, y, z] = chunks[x + xb, y + yb, z + zb];
+                    }
+                    else
+                    {
+                        chunks[x, y, z].transform.position += direction * chunkSize;
+                        chunks[x, y, z].GetComponent<chunk>().createMarchingBlock(chunkSize);
+                    }
+                }
+            }
+        }
+    }
+
+    bool inArray(Vector3 newChunkArrayPosition)
+    {
+        int x = (int)newChunkArrayPosition.x;
+        int y = (int)newChunkArrayPosition.y;
+        int z = (int)newChunkArrayPosition.z;
+
+        if (x < 0 || y < 0 || z < 0)
+            return false;
+        if (x >= viewRange || y >= viewRange || z >= viewRange)
+            return false;
+
+        return false;
     }
 }
