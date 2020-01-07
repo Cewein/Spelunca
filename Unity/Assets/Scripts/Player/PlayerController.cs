0bc5007406c440d7a57d57e92f0370113dddbcec
Tom <tahina.rakoto76@gmail.com>
2020-01-07 20:49:08 +0100

    Change test weapon scene to prepar big commit

--- Unity/Assets/Scripts/Player/PlayerController.cs
+++ Unity/Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MinerController
 {
@@ -32,4 +33,5 @@ public class PlayerController : MinerController
         isRunning(Input.GetButton(runInputName));
         isJumping(Input.GetButtonDown(jumpInputName));
     }
+    
 }
