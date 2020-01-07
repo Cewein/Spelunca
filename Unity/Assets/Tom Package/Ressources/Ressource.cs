bdd957127476e3bb51e6c96e5e605542804cfe0d
Tom <tahina.rakoto76@gmail.com>
2020-01-08 00:38:13 +0100

    Add GunGauge and Resource

---
+++ Unity/Assets/Tom Package/Ressources/Ressource.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+public enum Resource {normal,fire,water,plant};
+public class ResourceBlock : MonoBehaviour
+{
+    [SerializeField]
+    private int quantity = 5;
+
+    private Resource currentResource;
+
+    private Action<bool> isPicking;
+}
+
