53b9c3a5640d7857f340cbf5f779878dcb387f4a
Antoine Pavy <antoine.pavy92@gmail.com>
2020-01-13 12:50:54 +0100

    fog + graphicsManager

---
+++ Unity/Assets/graphics/GraphicsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicsManager : MonoBehaviour
+{
+    [SerializeField] private Color fogColor;
+    [SerializeField] private float startFogDistance;
+    [SerializeField] private float endFogDistance;
+    [SerializeField] private ChunkManager chunkManager;
+    [SerializeField] private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float distance = chunkManager.chunkSize * chunkManager.viewRange;
+        RenderSettings.fog = true;
+        RenderSettings.fogMode = FogMode.Linear;
+        RenderSettings.fogColor = fogColor;
+        RenderSettings.fogEndDistance = endFogDistance;
+        RenderSettings.fogStartDistance = startFogDistance;
+        mainCamera.backgroundColor = fogColor;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
