6969e33cf2362218533cd46ec65ee538658385e6
Cewein <pro.mnowak@gmail.com>
2020-01-24 00:08:20 +0100

    added comment for mesh generation, started normal generation #2
    after looking at some paper i need to look into the derivative of the density function, because i don't want to have to compute the whole mesh topoligy of the mesh i'm creating.
    
    https://www.researchgate.net/publication/220944287_Approximating_Normals_for_Marching_Cubes_applied_to_Locally_Supported_Isosurfaces
    
    this paper is good for reference

--- Unity/Assets/graphics/GraphicsManager.cs
+++ Unity/Assets/graphics/GraphicsManager.cs
@@ -7,24 +7,14 @@ public class GraphicsManager : MonoBehaviour
     [SerializeField] private Color fogColor;
     [SerializeField] private float startFogDistance;
     [SerializeField] private float endFogDistance;
-    [SerializeField] private ChunkManager chunkManager;
-    [SerializeField] private Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
-        float distance = chunkManager.chunkSize * chunkManager.viewRange;
         RenderSettings.fog = true;
         RenderSettings.fogMode = FogMode.Linear;
         RenderSettings.fogColor = fogColor;
         RenderSettings.fogEndDistance = endFogDistance;
         RenderSettings.fogStartDistance = startFogDistance;
-        mainCamera.backgroundColor = fogColor;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Camera.main.backgroundColor = fogColor;
     }
 }
--- Unity/Assets/worldgen/script/MeshGenerator.cs
+++ Unity/Assets/worldgen/script/MeshGenerator.cs
@@ -13,11 +13,12 @@ public class MeshGenerator
 
     public static MeshData generateMesh(float[,,] block, int x, int y, int z, uint size, float isolevel, int tcount)
     {
-
+        //create a new gridcell struct
         gridcell grid = new gridcell();
         grid.p = new Vector3[8];
         grid.val = new float[8];
 
+        //add to the gridcell the value of each point in the grid
         grid.val[0] = block[x, y, z];
         grid.val[1] = block[x + 1, y, z];
         grid.val[2] = block[x + 1, y, z + 1];
@@ -28,6 +29,7 @@ public class MeshGenerator
         grid.val[6] = block[x + 1, y + 1, z + 1];
         grid.val[7] = block[x, y + 1, z + 1];
 
+        //add to the gridcell the position of each point in the grid
         grid.p[0] = new Vector3(x, y, z);
         grid.p[1] = new Vector3(x + 1, y, z);
         grid.p[2] = new Vector3(x + 1, y, z + 1);
@@ -37,6 +39,7 @@ public class MeshGenerator
         grid.p[6] = new Vector3(x + 1, y + 1, z + 1);
         grid.p[7] = new Vector3(x, y + 1, z + 1);
 
+        //make it a vertex
         return polygonise(grid, isolevel, tcount);
                 
     }
@@ -46,6 +49,13 @@ public class MeshGenerator
         int cubeindex;
         Vector3[] vertlist = new Vector3[12];
 
+        // with cube index we manipulate
+        // each bit of the int variable
+        // if the value is less than the
+        // isolevel it flip the bit on
+        //
+        // it will be used for the poligonazing
+        // of each vertex
         cubeindex = 0;
         if (grid.val[0] < isolevel) cubeindex |= 1;
         if (grid.val[1] < isolevel) cubeindex |= 2;
@@ -56,6 +66,8 @@ public class MeshGenerator
         if (grid.val[6] < isolevel) cubeindex |= 64;
         if (grid.val[7] < isolevel) cubeindex |= 128;
 
+        //we look into the edge table and if the bit is flip on
+        //if it is we add to a vertlist a new vertex
         if ((LookUpTable.edgeTable[cubeindex] & 1) != 0)
             vertlist[0] =
                VertexInterp(isolevel, grid.p[0], grid.p[1], grid.val[0], grid.val[1]);
@@ -94,17 +106,20 @@ public class MeshGenerator
                VertexInterp(isolevel, grid.p[3], grid.p[7], grid.val[3], grid.val[7]);
 
         List<Vector3> vertices = new List<Vector3>();
+        List<Vector3> normals = new List<Vector3>();
         List<int> indices = new List<int>();
         int tcounttemp = 0;
 
+        //this is where we make the vertex and normal for the mesh
         for(int i = 0; LookUpTable.triTable[cubeindex,i] != -1; i +=3)
         {
             vertices.Add(vertlist[LookUpTable.triTable[cubeindex,i + 2]]);
             vertices.Add(vertlist[LookUpTable.triTable[cubeindex,i + 1]]);
             vertices.Add(vertlist[LookUpTable.triTable[cubeindex,i]]);
+
         }
 
-        return new MeshData(vertices, indices, tcounttemp);
+        return new MeshData(vertices, normals, indices, tcounttemp);
     }
 
     private static Vector3 VertexInterp(float isolevel, Vector3 p1, Vector3 p2, float valp1, float valp2)
@@ -125,25 +140,35 @@ public class MeshGenerator
 
         return p;
     }
+
+    private static Vector3 VextexNormal(Vector3 p)
+    {
+        Vector3 normal = new Vector3();
+
+        return normal;
+    }
 }
 
 public class MeshData
 {
     public List<Vector3> vertices;
+    public List<Vector3> normals;
     //public List<int> triangles;
     public int tcount;
 
     public MeshData()
     {
         vertices = new List<Vector3>();
+        normals = new List<Vector3>();
         //triangles = new List<int>();
         tcount = 0;
     }
 
-    public MeshData(List<Vector3> vertices, List<int> triangles, int tcount)
+    public MeshData(List<Vector3> vertices, List<Vector3> normals, List<int> triangles, int tcount)
     {
         this.vertices = vertices;
-       // this.triangles = triangles;
+        this.normals = normals;
+        //this.triangles = triangles;
         this.tcount = tcount;
     }
 
@@ -158,6 +183,7 @@ public class MeshData
     {
         Mesh mesh = new Mesh();
         mesh.vertices = vertices.ToArray();
+        //mesh.normals = normals.ToArray();
         mesh.triangles = Enumerable.Range(0, vertices.Count).ToArray();
         mesh.RecalculateNormals();
         return mesh;
