6591fab3f637ea98b4e1ddfd6ad25b04b675ad36
Tom <tahina.rakoto76@gmail.com>
2020-02-19 18:57:45 +0100

    add ring menu and gauge life

---
+++ Unity/Assets/Player/Scripts/PlayerStats.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [Tooltip("Player maximum life amount.")][SerializeField]
+    private float maxLife = 100;
+    private float life;
+    
+
+    public float Life { get => life; }
+
+    public float MaxLife{get => maxLife;}
+
+    public Action<float, Vector3, ResourceType> hurt;
+    public Action<float> heal;
+    public Action<bool> die;
+
+    private void Awake()
+    {
+        life = maxLife;
+    }
+
+    public void SetDamage(int damage, Vector3 direction, ResourceType damageType = ResourceType.normal)
+    {
+        hurt?.Invoke(damage, direction, damageType);
+        life -= damage;
+        if (life <= 0) isPlayerDie(true);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SetDamage(5, new Vector3(15,20+Time.deltaTime,0));
+        }
+    }
+    
+    public void RestoreLife(int hp)
+    {
+        heal?.Invoke(hp);
+        life += hp;
+    }
+    
+    private void isPlayerDie(bool isDie)
+    {
+        die?.Invoke(isDie);
+        life = 0;
+    }
+}
---
+++ Unity/Assets/UI/HUD/Scripts/LifeGauge.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class LifeGauge : MonoBehaviour
+{
+    [Tooltip("The player stats this gauge ui displayed content.")][SerializeField] 
+    private PlayerStats player = null;
+    private Image gaugeLifeUI;
+    
+    private void Start()
+    {
+        gaugeLifeUI = GetComponent<Image>();
+        gaugeLifeUI.fillAmount = player.Life / player.MaxLife;
+        //gaugeLifeUI.color = gunLoader.CurrentResource.Color;
+        
+
+        
+        player.hurt += (damage,_,__) =>
+        {
+            gaugeLifeUI.fillAmount -= damage / player.MaxLife;
+        };
+        player.heal += (hp) =>
+        {
+            gaugeLifeUI.fillAmount += hp / player.MaxLife;
+        }; 
+    }
+
+}
