31468f7187ed5216bf010b78f3d970d743d147d7
Tom <tahina.rakoto76@gmail.com>
2020-02-23 16:32:16 +0100

    return to main menu after dying

--- Unity/Assets/Game logic/GameManager.cs
+++ Unity/Assets/Game logic/GameManager.cs
@@ -1,41 +1,51 @@
-﻿using System.Collections;
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
     public static GameManager instance = null;
-    [SerializeField]
-    private ResourcesStock resourcesStock;
-
+    [SerializeField] private PlayerStats player = null;
+    [SerializeField] private GameObject gameOverScreen = null;
+    public string mainMenuPath;
+    public string gameScenePath;
 
     void Awake()
     {
-        DebugResourcesStockNotLoading();
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    private IEnumerator DebugResourcesStockNotLoading()
-    {
-        yield return new WaitForEndOfFrame();
         if (instance == null) instance = this;
         else if (instance != this) Destroy(gameObject);
         DontDestroyOnLoad(gameObject);
-      //  initRun();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        if (player != null) player.die += PlayerDie;
+        gameOverScreen.SetActive(false);
+
     }
 
     //Initializes the game for each run.
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     void initRun()
     {
-       // do global var initialisation for the current run.
-
-       if (ResourcesStock.Instance == null)
-       {
-           resourcesStock = ScriptableObject.CreateInstance<ResourcesStock>();
-       }
-
+      
     }
+
+    IEnumerator SetTimer(float t)
+    {
+        yield return new WaitForSeconds(t);
+    }
+
+    void PlayerDie(bool isDie)
+    {
+        gameOverScreen.SetActive(true);
+        SetTimer(2f);
+        LoadLevel(mainMenuPath);
+    }
+
+    void LoadLevel(string path)
+    {          
+        SceneManager.LoadScene(path);
+    }
+    
 
 }
