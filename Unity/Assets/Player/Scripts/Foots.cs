f5deb7f8482c4f0e8f4ebeb45da77df136eba0b1
Tom <tahina.rakoto76@gmail.com>
2020-03-24 17:43:18 +0100

    fix jump bug

--- Unity/Assets/Player/Scripts/Foots.cs
+++ Unity/Assets/Player/Scripts/Foots.cs
@@ -6,20 +6,15 @@ public class Foots : MonoBehaviour
 {
     public int additionalJumps = 0;
     private int maxJumps = 0;
-    private RaycastHit hit;
-    private Ray ray; y
+
     private void Awake()
     {
         maxJumps = additionalJumps;
-        ray = new Ray(transform.position, -transform.up);
     }
 
-    private void FixedUpdate()
+    private void OnTriggerEnter(Collider info)     
     {
-        ray.origin = transform.position;
-        if (!Physics.SphereCast(ray, 10,out hit, 5)) return;
         additionalJumps = maxJumps;
-        Debug.DrawRay(ray.origin,ray.direction,Color.yellow);
     }
-
+    
 }
