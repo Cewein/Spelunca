f5e24cbd9dd938fdbd70565de2e49911b29bb7cf
Tom <tahina.rakoto76@gmail.com>
2020-03-25 12:34:20 +0100

    new player

---
+++ Unity/Assets/Player/Scripts/MinerInputHandler.cs
@@ -0,0 +1,189 @@
+﻿using UnityEngine;
+
+public class MinerInputHandler : MonoBehaviour
+{
+    
+    [Header("Inputs")]
+    
+    [Tooltip("The run input name as it defined in Edit > Project Settings > Inputs Manager.")] [SerializeField]
+    private string runInputName = "Run";
+    
+    [Tooltip("The move horizontally input name as it defined in Edit > Project Settings > Inputs Manager.")] [SerializeField]
+    private string horizontalInputName = "Horizontal";
+    
+    [Tooltip("The move vertically input name as it defined in Edit > Project Settings > Inputs Manager.")] [SerializeField]
+    private string verticalInputName = "Vertical";
+    
+    [Tooltip("The rotate horizontally input name as it defined in Edit > Project Settings > Inputs Manager.")] [SerializeField]
+    private string azimuthInputName = "Mouse X";
+    
+    [Tooltip("The rotate vertically input name as it defined in Edit > Project Settings > Inputs Manager.")] [SerializeField]
+    private string elevationInputName = "Mouse Y";
+    
+    [Tooltip("The jump input name as it defined in Edit > Project Settings > Inputs Manager.")] [SerializeField]
+    private string jumpInputName = "Jump";
+    
+    [Tooltip("The grapping hook input name as it defined in Edit > Project Settings > Inputs Manager.")] [SerializeField]
+    private string grapplingInputName = "Grappling";
+    
+    [Tooltip("The fire input name as it defined in Edit > Project Settings > Inputs Manager.")] [SerializeField]
+    private string fireInputName = "Fire";
+    
+    [Tooltip("The aim input name as it defined in Edit > Project Settings > Inputs Manager.")] [SerializeField]
+    private string aimInputName = "Aim";
+    
+    [Tooltip("The crouch input name as it defined in Edit > Project Settings > Inputs Manager.")] [SerializeField]
+    private string crouchInputName = "Cancel";
+    
+    [Tooltip("The switch weapon input name as it defined in Edit > Project Settings > Inputs Manager.")] [SerializeField]
+    private string switchWeaponInputName = "Mouse Scrollwheel";
+    
+    [Tooltip("Sensitivity multiplier for moving the camera around")]
+    public float lookSensitivity = 1f;
+    [Tooltip("Limit to consider an input when using a trigger on a controller")]
+    public float triggerAxisThreshold = 0.4f;
+ 
+
+    bool m_FireInputWasHeld;
+    private Vector3 move;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void LateUpdate()
+    {
+        m_FireInputWasHeld = GetFireInputHeld();
+    }
+
+    public bool CanProcessInput()
+    {
+        return Cursor.lockState == CursorLockMode.Locked;
+    }
+
+    public Vector3 GetMoveInput()
+    {
+        if (!CanProcessInput()) return Vector3.zero;
+        move.x = Input.GetAxisRaw(horizontalInputName);
+        move.y = 0f;
+        move.z = Input.GetAxisRaw(verticalInputName);
+
+        // constrain move input to a maximum magnitude of 1, otherwise diagonal movement might exceed the max move speed defined
+        move = Vector3.ClampMagnitude(move, 1);
+
+        return move;
+
+    }
+
+    public float GetLookInputsHorizontal()
+    {
+        return GetMouseOrStickLookAxis(azimuthInputName, azimuthInputName);
+    }
+
+    public float GetLookInputsVertical()
+    {
+        return GetMouseOrStickLookAxis(elevationInputName, elevationInputName);
+    }
+
+    public bool GetJumpInputDown()
+    {
+        if (CanProcessInput())
+        {
+            return Input.GetButtonDown(jumpInputName);
+        }
+
+        return false;
+    }
+
+    public bool GetJumpInputHeld()
+    {
+        if (CanProcessInput())
+        {
+            return Input.GetButton(jumpInputName);
+        }
+
+        return false;
+    }
+
+    public bool GetFireInputDown()
+    {
+        return GetFireInputHeld() && !m_FireInputWasHeld;
+    }
+
+    public bool GetFireInputReleased()
+    {
+        return !GetFireInputHeld() && m_FireInputWasHeld;
+    }
+
+    public bool GetFireInputHeld()
+    {
+        return CanProcessInput() && Input.GetButton(fireInputName);
+    }
+
+    public bool GetAimInputHeld()
+    {
+        return CanProcessInput() && Input.GetButton(aimInputName);
+    }
+
+    public bool GetSprintInputHeld()
+    {
+        return CanProcessInput() && Input.GetButton(runInputName);
+    }
+
+    public bool GetCrouchInputDown()
+    {
+        return CanProcessInput() && Input.GetButtonDown(crouchInputName);
+    }
+
+    public bool GetCrouchInputReleased()
+    {
+        return CanProcessInput() && Input.GetButtonUp(crouchInputName);
+    }
+
+    public int GetSwitchWeaponInput()
+    {
+        if (CanProcessInput())
+        {
+
+
+            if (Input.GetAxis(switchWeaponInputName) > 0f)
+                return -1;
+            if (Input.GetAxis(switchWeaponInputName) < 0f)
+                return 1;
+        }
+
+        return 0;
+    }
+    
+    float GetMouseOrStickLookAxis(string mouseInputName, string stickInputName)
+    {
+        if (CanProcessInput())
+        {
+            // Check if this look input is coming from the mouse
+            bool isGamepad = Input.GetAxis(stickInputName) != 0f;
+            float i = isGamepad ? Input.GetAxis(stickInputName) : Input.GetAxisRaw(mouseInputName);
+
+            if (mouseInputName.Equals(elevationInputName)) i *= -1f;
+            // apply sensitivity multiplier
+            i *= lookSensitivity;
+
+            if (isGamepad)
+            {
+                // since mouse input is already deltaTime-dependant, only scale input with frame time if it's coming from sticks
+                i *= Time.deltaTime;
+            }
+            else
+            {
+                // reduce mouse input amount to be equivalent to stick movement
+                i *= 0.01f;
+
+            }
+
+            return i;
+        }
+
+        return 0f;
+    }
+}
---
+++ Unity/Assets/Player/Scripts/old/PlayerPhysic.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+/*
+[RequireComponent(typeof(MinerController))]
+public class PlayerPhysic : MonoBehaviour
+{
+    #region SerializeFields ==========
+
+    [Header("Configuration")]
+    [Tooltip("The player walk speed in unit per frame.")] [SerializeField]
+    private float walkSpeed = 150f;
+    
+    [Tooltip("The player run speed in unit per frame.")][SerializeField]
+    private float runSpeed = 500f;
+    
+    [Tooltip("The player jump force in unit per frame.")][SerializeField]
+    private float jumpForce = 500f;
+    
+    [Header("Colliders parameters")]
+    [Tooltip("The box collider used to detect if the player is on ground and can jump.")] [SerializeField]
+    private Foots footCollider = null;
+    [Header("Grappling Hook parameters")]
+    [Tooltip("The origin of the grappling hook.")] [SerializeField]
+    private Hook HookPrefab;
+    [Tooltip("The origin of the grappling hook.")] [SerializeField]
+    private Transform GrapplingOrigin;
+    [Tooltip("The player's camera.")][SerializeField]
+    private Camera camera;
+    [Tooltip("ONLY FOR FRAME DEBUG")][SerializeField]
+    private bool launchGrapplingHook;
+    #endregion
+    
+    #region Fields ==========
+
+    private MinerController minerController = null;
+    private Vector3 newVelocity = Vector3.zero;
+    private Vector3 jumpVelocity = Vector3.zero;
+    private float currentSpeed = 0;
+    private Rigidbody rb;
+    private Hook hook;
+    
+    //Grappling hook variables
+    private bool previousGrappingInput = false;//The grappling hook's input during the previous state
+    #endregion
+    
+    public void Awake()
+    {
+        minerController = GetComponent<MinerController>();
+        rb = gameObject.GetComponent<Rigidbody>();
+
+        minerController.move += move;
+        minerController.jump += isJumping => { jump(isJumping);};
+        minerController.grapplingHook += isGrappling => { grapplingHook(isGrappling);};
+        minerController.run += isRunning => { run(isRunning);};
+        hook = Instantiate(HookPrefab, transform.position, transform.rotation);
+        hook.origin = GrapplingOrigin;
+    }
+    
+    void FixedUpdate(){setVelocity();}
+
+    private void move(float xMov, float zMov)
+    {
+        Vector3 moveHorizontal = transform.right * xMov;
+        Vector3 moveVertical = transform.forward * zMov;
+        newVelocity = (moveHorizontal + moveVertical).normalized * currentSpeed;
+    }
+
+    private bool run(bool isRunning)
+    {
+        currentSpeed = isRunning ? runSpeed : walkSpeed;
+        return isRunning;
+    }
+
+    private bool jump(bool isJumping)
+    {
+       
+        if (isJumping && footCollider.additionalJumps >= 0)
+        {
+            jumpVelocity = transform.up * (jumpForce * rb.mass);
+            footCollider.additionalJumps--;
+        }
+        else jumpVelocity = Vector3.zero;
+        return isJumping;
+    }
+    
+    private bool grapplingHook(bool isGrappling)
+    {
+        bool grapplingControl = launchGrapplingHook || isGrappling; //FIXME: Will be removed
+        if (previousGrappingInput == true && grapplingControl == false)//Le joueur a relaché la touche, on doit arreter le grappin
+        {
+            hook.state = GrapplingHookState.Retracting;
+            
+        }else if (previousGrappingInput == false && grapplingControl == true && hook.state != GrapplingHookState.Retracting)//Le vient d'appuyer sur le bouton, on doit déployer le grappin
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(GrapplingOrigin.position,camera.transform.forward,out hit,hook.maxDeployDistance)){
+                hook.state = GrapplingHookState.Expanding;
+                hook.renderer.enabled = true;
+                
+                hook.origin = GrapplingOrigin;
+                hook.rope.origin = GrapplingOrigin;
+                hook.transform.position = GrapplingOrigin.position;
+                hook.target = hit.point;
+                hook.player = rb;
+            }
+        }
+        previousGrappingInput = grapplingControl;
+        return grapplingControl;
+    }
+    
+    void setVelocity()
+    {
+        if (hook.state != GrapplingHookState.Pulling)
+        {
+            rb.velocity = transform.up * rb.velocity.y + newVelocity * Time.fixedDeltaTime;
+            rb.AddForce(jumpVelocity);
+        }
+    }
+
+   
+}
+*/
