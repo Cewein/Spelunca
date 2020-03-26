4c39267aa6dd68e880a65bef9441ea8c46b2dda6
Tom <tahina.rakoto76@gmail.com>
2020-03-26 20:04:26 +0100

    don't aim with pickaxe

--- Unity/Assets/Player/Scripts/MinerController.cs
+++ Unity/Assets/Player/Scripts/MinerController.cs
@@ -158,7 +158,7 @@ public class MinerController : MonoBehaviour
 
     private void Aim()
     {
-        
+        if(weaponIndex != 0 ) return;
         weaponNewPosition = minerInputs.isAiming(true) ?
                                                  weaponsAimingPosition.position : weaponDefaultPosition.position;
         weaponParent.position = Vector3.Slerp(weaponParent.position, weaponNewPosition, aimingAcceleration * Time.deltaTime);
