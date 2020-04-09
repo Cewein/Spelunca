fe28c1cd1e76c8870324dd9278abc79f10c287eb
Antoine Pavy <antoine.pavy92@gmail.com>
2020-04-09 21:27:30 +0200

    new ChatManager and CommandManager
    added "tp" and "destroy" commands

---
+++ Unity/Assets/UI/Chat/ChatManager.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatManager : MonoBehaviour
+{
+    [SerializeField][InputName]
+    private string chatInputName;
+    [SerializeField] private CommandManager commandManager;
+    
+    [SerializeField] private int maxMessage = 25;
+
+    [SerializeField] private GameObject chat;
+    [SerializeField] private GameObject chatContent;
+    [SerializeField] private GameObject chatTextPrefab;
+    [SerializeField] private InputField chatBox;
+    
+    
+    [Header("Message colors")]
+    [SerializeField] private Color playerMessageColor = Color.white;
+    [SerializeField] private Color commandColor = Color.green;
+    [SerializeField] private Color infoColor = Color.cyan;
+    [SerializeField] private Color warningColor = Color.yellow;
+    [SerializeField] private Color errorColor = Color.red;
+    
+    [SerializeField] private List<Message> messageList = new List<Message>();
+
+    private string previousLine = "";
+
+    private bool buttonPressed;
+    void Start()
+    {
+        chat.SetActive(false);
+    }
+    void Update()
+    {
+        if (chat.activeInHierarchy)
+        {
+            if (chatBox.isFocused)
+            {
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    chatBox.text = previousLine;
+                }
+            }
+            if (chatBox.text != "")
+            {
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    previousLine = chatBox.text;
+                    ParseMessage(chatBox.text);
+                    chatBox.text = "";
+                    chatBox.ActivateInputField();
+                }
+            }
+            else if(!chatBox.isFocused && Input.GetKeyDown(KeyCode.Return)){
+                chatBox.ActivateInputField();
+            }
+        }
+        
+        if (!chatBox.isFocused)
+        {
+            if (Input.GetAxis(chatInputName) > 0)
+            {
+                if (!buttonPressed)
+                    chat.SetActive(!chat.activeInHierarchy);
+                buttonPressed = true;
+                chatBox.ActivateInputField();
+            }
+            else
+            {
+                if (buttonPressed)
+                    buttonPressed = false;
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SendMessageToChat("You pressed the space bar !",Message.Type.info);
+            }
+        }
+        
+    }
+
+    private Color MessageTypeColor(Message.Type type)
+    {
+         Color color = playerMessageColor;
+
+         switch (type)
+         {
+             case Message.Type.info:
+                 color = infoColor;
+                 break;
+             case Message.Type.playerMessage:
+                 color = playerMessageColor;
+                 break;
+             case Message.Type.command:
+                 color = commandColor;
+                 break;
+             case Message.Type.warning:
+                 color = warningColor;
+                 break;
+             case Message.Type.error:
+                 color = errorColor;
+                 break;
+         }
+
+         return color;
+    }
+
+    public void SendMessageToChat(string text,Message.Type type)
+    {
+        if (messageList.Count >= maxMessage)
+        {
+            Destroy(messageList[0].textObject.gameObject);
+            messageList.Remove(messageList[0]);
+        }
+        Message newMessage = new Message();
+        newMessage.text = text;
+        GameObject newText = Instantiate(chatTextPrefab, chatContent.transform);
+        newMessage.textObject = newText.GetComponent<Text>();
+        newMessage.textObject.text = newMessage.text;
+        newMessage.textObject.color = MessageTypeColor(type);
+        messageList.Add(newMessage);
+    }
+
+    private void ParseMessage(String text)
+    {
+        
+        if (text[0] == '/')
+        {
+            CommandMessage msg = commandManager.parseCommand(text.Substring(1));
+            
+            if (!msg.isError)//si on a pas eu d'erreur
+            {
+                SendMessageToChat(msg.message,Message.Type.command);
+            }
+            else
+            {
+                SendMessageToChat("CommandError : "+msg.message,Message.Type.error);
+            }
+        }
+        else
+        {
+            SendMessageToChat(text,Message.Type.playerMessage);
+        }
+    }
+}
---
+++ Unity/Assets/UI/Chat/CommandManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[Serializable] public class GODictionary : SerializableDictionary<string, GameObject> { }
+
+public class CommandMessage
+{
+    public bool isError;
+    public string message;
+}
+
+public class CommandManager : MonoBehaviour
+{
+    
+    private string[] cmdWords;
+    
+    [SerializeField] public GODictionary targetableObjects;
+    public CommandMessage parseCommand(string command)
+    {
+        cmdWords = command.Split(' ');
+
+        switch (cmdWords[0])
+        {
+            case "tp":
+                return cmdTp();
+            case "destroy":
+                return cmdDestroy();
+        }
+        return commandError("unknown command.");
+    }
+
+    private CommandMessage cmdTp()
+    {
+        if (cmdWords.Length == 3) //mode target, destination
+        {
+            string targetKey = cmdWords[1];
+            string destinationKey= cmdWords[2];
+            if(targetableObjects.ContainsKey(targetKey))
+            {
+                if(targetableObjects.ContainsKey(destinationKey))
+                {
+                    GameObject target = targetableObjects[targetKey];
+                    GameObject destination = targetableObjects[destinationKey];
+                    if (target != null)
+                    {
+                        if (destination != null)
+                        {
+                            target.transform.position = destination.transform.position;
+                            return commandSuccess("'" + targetKey + "' teleported to '" + destinationKey + "'.");
+                        }
+                        return commandError("'" + destinationKey + "' doesn't exist anymore.");
+                    }
+                    return commandError("'" + targetKey + "' doesn't exist anymore.");
+                } 
+                return commandError("unknown destination : '"+destinationKey+"'.");
+            }
+            return commandError("unknown target : '"+targetKey+"'.");
+            
+        }
+        else if (cmdWords.Length == 2) //mode player to destination
+        {
+            
+        }
+        return commandError("tp command requires at least 1 parameter.");
+    }
+    
+    private CommandMessage cmdDestroy()
+    {
+        if (cmdWords.Length == 2) //mode target, destination
+        {
+            string toDestroyKey = cmdWords[1];
+            if(targetableObjects.ContainsKey(toDestroyKey))
+            {
+                GameObject toDestroy = targetableObjects[toDestroyKey];
+                if (toDestroy != null)
+                {
+                    Destroy(toDestroy);
+                    //targetableObjects.Remove(toDestroyKey);
+                    return commandSuccess("'" + toDestroyKey + "' destroyed.");
+                }
+                return commandError("'" + toDestroyKey + "' has already been destroyed.");
+            }
+            return commandError("unknown target : '"+toDestroyKey+"'.");
+        }
+        return commandError("destroy command requires 1 parameter.");
+    }
+
+    private GameObject getTarget(string key)
+    {
+        if (targetableObjects[key] == null)
+        {
+            Debug.Log("null");
+            return null;
+        }
+        return targetableObjects[key];
+    }
+    private CommandMessage commandSuccess(string message)
+    {
+        this.cmdWords = new string[0]; //pour éviter des potentiels résidus on vide la liste de mots
+        CommandMessage success = new CommandMessage();
+        success.message = message;
+        return success;
+    }
+    private CommandMessage commandError(string message)
+    {
+        this.cmdWords = new string[0]; //pour éviter des potentiels résidus on vide la liste de mots
+        CommandMessage error = new CommandMessage();
+        error.isError = true;
+        error.message = message;
+        return error;
+    }
+}
