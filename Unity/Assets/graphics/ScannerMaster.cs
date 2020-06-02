f4ac6842b5ea1a092d831880b36d428ffbe444ed
Cewein <pro.mnowak@gmail.com>
2020-06-02 22:47:36 +0200

    moving scanner

--- Unity/Assets/graphics/ScannerMaster.cs
+++ Unity/Assets/graphics/ScannerMaster.cs
@@ -3,18 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
-public class CelShadingMaster : MonoBehaviour
+public class ScannerMaster : MonoBehaviour
 {
-    public Shader celShadingShader;
-    public float dist;
+    public Shader scannerShader;
     public float width;
+    public KeyCode key;
+    public float speed;
+
     private Material _mat;
+    private float dist = -1.0f;
+    private bool isMoving = false;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            isMoving = true;
+            dist = -1.0f;
+        }
+
+        if (dist < 150.0 && isMoving)
+            dist += speed;
+        else
+        {
+            dist = -1.0f;
+            isMoving = false;
+        }
+    }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         if(_mat == null)
         {
-            _mat = new Material(celShadingShader);
+            _mat = new Material(scannerShader);
             _mat.SetVector("iResolution", new Vector2(Screen.width, Screen.height));
         }
 
