2b1ce716302cf78de225d0936583f3b288c817c5
Antoine Pavy <antoine.pavy92@gmail.com>
2020-06-07 16:33:20 +0200

    LoosingScreen can show main menu

---
+++ Unity/Assets/UI/HUD/LoosingScreen/LoosingScriptManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoosingScriptManager : MonoBehaviour
+{
+    public int mainMenuIndex = 0;
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuIndex);
+    }
+}
