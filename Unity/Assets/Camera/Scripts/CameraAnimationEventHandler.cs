9d551fc2085cc24c5fffdb2606a92ee4f6fa3096
Tom <t.rakoto76@gmail.com>
2020-06-13 10:47:32 +0200

    breathing

--- Unity/Assets/Camera/Scripts/CameraAnimationEventHandler.cs
+++ Unity/Assets/Camera/Scripts/CameraAnimationEventHandler.cs
@@ -1,15 +1,55 @@
-﻿using UnityEngine;
-[RequireComponent(typeof(Animator))]
+using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+[RequireComponent(typeof(Camera))]
 public class CameraAnimationEventHandler : MonoBehaviour
 {
-    [Header("Configuration")] [Tooltip("The camera animator.")] [SerializeField]
-    private Animator animator = null;
+    [Header("Controller")]
+    [SerializeField]
     private MinerInputHandler inputHandler = null;
+    
+    [Header("Emotion")]
+    [SerializeField][Range(1,3)] private float fear = 0;
+ //   [SerializeField][Range(0,1)] private float health = 1; // TODO : caméra plus instable si peu de vie
+
+    [Header("Breath")]
+    [SerializeField] private float minHeight = .7f;
+    [SerializeField] private float maxHeight = .9f;
+
+    private float breathingMovement;
+
+    private bool breathIn;
+    //[Header("Run")]
+    //[Header("Jump")]
+    
     private void Start()
     {
-        inputHandler = GetComponentInParent<MinerInputHandler>();
-        animator = GetComponent<Animator>();
-        animator.SetBool("isRunning", inputHandler.isRunning()); 
-        animator.SetBool("isJumping", inputHandler.isJumping()); 
+        //inputHandler = GetComponentInParent<MinerInputHandler>();
+        /*animator.SetBool("isRunning", inputHandler.isRunning()); 
+        animator.SetBool("isJumping", inputHandler.isJumping()); */
     }
+
+    private void Update()
+    {
+        Breath();
+    }
+
+    private void Breath()
+    {
+        if (breathIn)
+        {
+            breathingMovement = Mathf.Lerp(breathingMovement, maxHeight, Time.deltaTime*  1.7f * fear);
+            transform.localPosition = new Vector3(transform.localPosition.x,breathingMovement,transform.localPosition.z);
+            if (breathingMovement >= maxHeight -.005f) breathIn = false;
+        }
+        else
+        {
+            breathingMovement = Mathf.Lerp(breathingMovement, minHeight, Time.deltaTime* 1.7f * fear);
+            transform.localPosition = new Vector3(transform.localPosition.x,breathingMovement,transform.localPosition.z);
+            if (breathingMovement <= minHeight +.005f) breathIn = true;
+        }
+
+    }
+
 }
