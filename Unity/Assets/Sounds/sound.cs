21f97b489b308c383868d208e172ef3eefe62cd8
Cewein <pro.mnowak@gmail.com>
2020-06-27 17:04:04 +0200

    added UI Click sound Main menu

---
+++ Unity/Assets/Sounds/sound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound : MonoBehaviour
+{
+
+    public AudioSource uiClick;
+
+    public void playButtonClick()
+    {
+        uiClick.Play();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
