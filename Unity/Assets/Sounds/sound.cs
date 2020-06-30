51f41b04d26c9380c0c8e1d2838eb7bdd20c9bb4
Cewein <pro.mnowak@gmail.com>
2020-06-30 02:29:32 +0200

    ig musique

--- Unity/Assets/Sounds/sound.cs
+++ Unity/Assets/Sounds/sound.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class sound : MonoBehaviour
 {
-
+    [Header("sources")]
     public AudioSource uiClick;
+    public AudioSource[] gameMusic;
+
+    [Header("Config")]
+    [Range(0, 1)]
+    public float igVolume= 0.2f;
+
+    private int selectedMusic = 0;
+
 
     public void playButtonClick()
     {
@@ -14,12 +22,25 @@ public class sound : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        selectedMusic = Random.Range(0, gameMusic.Length);
+        gameMusic[selectedMusic].volume = igVolume;
+        gameMusic[selectedMusic].Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!gameMusic[selectedMusic].isPlaying)
+        {
+            int temp = selectedMusic;
+            while(temp == selectedMusic)
+            {
+                temp = Random.Range(0, gameMusic.Length);
+            }
+            selectedMusic = temp;
+            
+            gameMusic[selectedMusic].Play();
+        }
+        gameMusic[selectedMusic].volume = igVolume;
     }
 }
