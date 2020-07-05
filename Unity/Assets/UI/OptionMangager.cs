e5e69c2ce65945e160c783ac25bd83f993cc54a7
Cewein <pro.mnowak@gmail.com>
2020-07-05 19:22:14 +0200

    default option worldgen menu

---
+++ Unity/Assets/UI/OptionMangager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionMangager : MonoBehaviour
+{
+
+    public OptionSlider[] worldgenSlider;
+
+    // Start is called before the first frame update 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < worldgenSlider.Length; i++)
+        {
+            worldgenSlider[i].value.text = worldgenSlider[i].slider.value.ToString();
+        }
+    }
+
+    public void setDefaultWorldGen()
+    {
+        for (int i = 0; i < worldgenSlider.Length; i++)
+        {
+            worldgenSlider[i].slider.value = worldgenSlider[i].defaultValue;
+        }
+    }
+}
+
+[System.Serializable]
+public struct OptionSlider
+{
+    public Text name;
+    public Text value;
+    public Slider slider;
+    public float defaultValue;
+}
